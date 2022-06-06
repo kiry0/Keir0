0fbb0ece37b5252e46432e87257333e24434e26d
Carl Hansen <this.carlhansen@gmail.com>
2022-06-06 20:52:25 +0200

    Update auth.js

--- src/schemas/auth.js
+++ src/schemas/auth.js
@@ -4,17 +4,26 @@ const register = joi.object(
     {
         firstName: joi.string().required(),
         lastName: joi.string().required(),
-        emailAddress: joi.string().email().lowercase().required(),
+        emailAddress: joi.string().email().lowercase(),
+        phoneNumber: joi.string().length(8).pattern(/^\d+$/).when("emailAddress", {
+            is: "",
+            then: joi.required()
+        }),
         username: joi.string().min(4).required(),
         password: joi.string().min(9).required()
     }
 );
 
 const login = joi.object({
-    emailAddress: joi.string().email().lowercase().empty(''),
-    username: joi.string().min(4).when('email',
+    emailAddress: joi.string().email().lowercase(),
+    phoneNumber: joi.string().length(8).pattern(/^\d+$/),
+    username: joi.string().min(4).when("emailAddress",
         { 
-            is: '', then: joi.required()
+            is: "",
+            then: joi.when("phoneNumber", {
+                is: "",
+                then: joi.required()
+            })
         }
     ),
     password: joi.string().min(9).required()
