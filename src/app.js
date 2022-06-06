14b60c604d792386c1041bdca85edfe252958aab
Carl Hansen <this.carlhansen@gmail.com>
2022-06-06 20:52:41 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,4 +10,7 @@ const server = new Server({
     mongodbURI: process.env.MONGODB_URI
 });
 
-server.start();
+server
+      .start()
+      .then(res => console.log(res))
+      .catch(err => console.error(err));
