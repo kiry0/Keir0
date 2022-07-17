d2204a55447fb7383adefb57e88f6c9b7173aaf3
Carl Hansen <this.carlhansen@gmail.com>
2022-07-17 20:28:54 +0200

    + logout route

--- src/lib/config/routes.js
+++ src/lib/config/routes.js
@@ -1,10 +1,12 @@
 const register = require("../../routes/auth/register.js")
     , logIn = require("../../routes/auth/logIn.js")
-    , verify = require("../../routes/auth/verify.js");
+    , verify = require("../../routes/auth/verify.js")
+    , logOut = require("../../routes/auth/logOut.js");
 
 module.exports = (fastify) => {
     fastify
            .register(register)
            .register(logIn)
-           .register(verify);
+           .register(verify)
+           .register(logOut);
 };
