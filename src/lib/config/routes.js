85f47c91a9838981033020bc79028096863deb9f
Carl Hansen <this.carlhansen@gmail.com>
2022-07-17 20:41:01 +0200

    + refreshToken route

--- src/lib/config/routes.js
+++ src/lib/config/routes.js
@@ -1,7 +1,8 @@
 const register = require("../../routes/auth/register.js")
     , logIn = require("../../routes/auth/logIn.js")
     , verify = require("../../routes/auth/verify.js")
-    , logOut = require("../../routes/auth/logOut.js");
+    , logOut = require("../../routes/auth/logOut.js")
+    , refreshToken = require("../../routes/auth/refreshToken.js");
 
 module.exports = (fastify) => {
     fastify
