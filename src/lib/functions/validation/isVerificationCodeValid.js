8624f8cdea24c8258aeb75a50a321c1178d24b12
Carl Hansen <this.carlhansen@gmail.com>
2022-07-18 18:16:52 +0200

    :sparkles: Feat: Checks if `verificationCode` exists and hasn't expired

--- src/lib/functions/validation/isVerificationCodeValid.js
+++ src/lib/functions/validation/isVerificationCodeValid.js
@@ -1,22 +1,22 @@
 const User = require("../../../models/User.js");
 
-async function isVerificationCodeValid(value, helpers) {
+const JoiExternalValidationError = require("../../classes/JoiExternalValidationError.js");
+
+const isVerificationCodeValid = async (value, helpers) => {
     try {
         const user = await User.findOne({ "verification.code.value": value });
 
-        const error = new Error();
-        
-        error.name = "JoiExternalError";
-        error.message = "Unable to verify your account! A user with that verificationCode does not exist or is invalid!";
-        error.statusCode = 410;
-    
-        if(!user) throw error;
+        if(!user) throw new JoiExternalValidationError("Unable to verify your account! A user with that verificationCode does not exist!");
 
-        // value = "Hello, World!";
-        // console.log("verificationCode:", value);
+        const isVerificationCodeExpired = (verificationCode) => {
+            // { milliseconds / 3.6e6 } returns hour/hours.
+            return Math.floor(Math.abs(new Date() - verificationCode.expiresAt) / 3.6e6) <= 0 ? true : false;
+        };
+
+        if(isVerificationCodeExpired(user.verification.code)) throw new JoiExternalValidationError("It looks like your verificationCode expired!");
 
         return value;
-    } catch (err) {
+    } catch(err) {
         console.error(err);
 
         return err;
