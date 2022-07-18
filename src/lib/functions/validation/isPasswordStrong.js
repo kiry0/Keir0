87c38f3f7d7abdec7770d38cb1131c73a770768c
Carl Hansen <this.carlhansen@gmail.com>
2022-07-18 18:30:38 +0200

    :truck: Rename

--- src/lib/functions/validation/isPasswordStrong.js
+++ src/lib/functions/validation/isPasswordStrong.js
@@ -1,9 +1,9 @@
 const zxcvbn = require("zxcvbn");
 
-const passwordStrength = (value, helpers) => {
+const isPasswordStrong = (value, helpers) => {
     if(zxcvbn(value).score <= 2) return helpers.error("any.custom");
 
     return value;
 };
 
-module.exports = passwordStrength;
+module.exports = isPasswordStrong;
