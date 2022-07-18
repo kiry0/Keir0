5630c7405f4960bc033041eb4a82288de83e6a5d
Carl Hansen <this.carlhansen@gmail.com>
2022-07-18 18:58:22 +0200

    :sparkles: Feat: Returns `phoneNumber` object now

--- src/lib/functions/validation/isPhoneNumberValid.js
+++ src/lib/functions/validation/isPhoneNumberValid.js
@@ -1,12 +1,19 @@
 const {
     isValidNumber,
-    isPossiblePhoneNumber
+    isPossiblePhoneNumber,
+    parsePhoneNumber
 } = require("libphonenumber-js");
 
 const isPhoneNumberValid = (phoneNumber, helpers) => { 
     const isPhoneNumberValid = isValidNumber(phoneNumber) || isPossiblePhoneNumber(phoneNumber);
 
     if(!isPhoneNumberValid) return helpers.error("any.custom");
+
+    phoneNumber = {
+        countryCallingCode,
+        nationalNumber,
+        number
+    } = parsePhoneNumber(phoneNumber);
 
     return phoneNumber;
 };
